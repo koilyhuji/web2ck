67643a04b148cda90a163119176b6f54af67c835
bobby-stanoff <hongnguyen210180@gmail.com>
2025-05-19 09:10:19 -0400

    somehting thieuog

---
+++ src/test/java/com/pgq/template/TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgq.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
